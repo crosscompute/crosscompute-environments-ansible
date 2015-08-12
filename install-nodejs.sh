eb009dfa71baa47c8a93f5116cd0f0cfb2423b70
Roy Hyunjin Han <rhh@crosscompute.com>
2015-08-11 22:21:59 -0400

    Install nodejs via bash

---
+++ install-nodejs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ~/.virtualenvs/crosscompute/bin/activate
+export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:/usr/local/lib
+export NODE_PATH=$VIRTUAL_ENV/lib/node_modules
+mkdir $VIRTUAL_ENV/opt/node && cd $_
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+./configure --prefix=$VIRTUAL_ENV
+make install
+npm install -g browserify gulp gulp-if gulp-uglify http-proxy node-inspector socket.io vinyl-transform yargs
