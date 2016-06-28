c5938c15c8ce32784217e8e2730149fa4462ad39
Roy Hyunjin Han <rhh@crosscompute.com>
2016-06-28 08:59:01 -0400

    Let user configure virtualenv

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo yum -y install yum
 sudo yum -y install ansible
 sudo yum -y install redhat-rpm-config
 sudo yum -y install python-virtualenv
-ansible-playbook -i "localhost," -c local \
+ansible-playbook -e "$@" -i "localhost," -c local \
     base.yml \
     web.yml \
     numerical.yml \
