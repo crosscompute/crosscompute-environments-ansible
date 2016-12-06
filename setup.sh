f3655a5514ccb6f82ccabfebabedb39b42633109
Roy <rhh@crosscompute.com>
2016-12-06 18:48:40 -0500

    Replace tabs with spaces

--- setup.sh
+++ setup.sh
@@ -6,23 +6,20 @@ case "$1" in
 -u)
     REMOTE_USER="$2"
     shift
-	;;
+    ;;
 -i)
     REMOTE_HOST="${2//,}"
     shift
-	;;
+    ;;
 esac
 shift
 done
 
 case $REMOTE_OPERATING_SYSTEM in
 Fedora)
-	# if [ -n "$REMOTE_HOST" ]; then
-		# ssh ${REMOTE_USER:-$USER}@$REMOTE_HOST dnf install -y python
-	# fi
     sudo dnf -y install ansible
     ANSIBLE_OPTIONS+=" bootstrap.yml"
-	;;
+    ;;
 esac
 
 ansible-playbook -i hosts $ANSIBLE_OPTIONS site.yml
