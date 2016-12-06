2f8060797785f1a530fb98eccabda08e855e63ef
Roy <rhh@crosscompute.com>
2016-12-06 13:26:56 -0500

    Restore base packages

--- setup.sh
+++ setup.sh
@@ -3,12 +3,7 @@ OPERATING_SYSTEM=$(lsb_release -si)
 case $OPERATING_SYSTEM in
 
 Fedora)
-    HOST_URL=https://download1.rpmfusion.org
-    X=$(rpm -E %fedora)
-    sudo dnf -y install \
-        ansible \
-        $HOST_URL/free/fedora/rpmfusion-free-release-$X.noarch.rpm \
-        $HOST_URL/nonfree/fedora/rpmfusion-nonfree-release-$X.noarch.rpm
+    sudo dnf -y install ansible
     ANSIBLE_OPTIONS+=bootstrap.yml
 ;;
 
