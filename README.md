8c00fc9f22cb1aefd6607c07afcf3144e16982c8
Roy <rhh@crosscompute.com>
2016-12-06 16:49:45 -0500

    Move virtualenv to group_vars

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-Scientific Computing Environments via Ansible
-=============================================
+# Scientific Computing Environments via Ansible
+
 Here are [Ansible](http://www.ansible.com) playbooks for preparing a scientific computing environment.
 
     git clone https://github.com/invisibleroads/crosscompute-environments-ansible
     cd crosscompute-environments-ansible
+    bash setup.sh
+
+## Specify a different virtual environment
+
     bash setup.sh --extra-vars "virtualenv=~/.virtualenvs/crosscompute"
+
+## Specify a different host
+
+    bash setup.sh -u root -i 104.236.57.99,
--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
-OPERATING_SYSTEM=$(lsb_release -si)
+REMOTE_OPERATING_SYSTEM=${REMOTE_OPERATING_SYSTEM:-$(lsb_release -si)}
+ANSIBLE_OPTIONS+=$@
 
-case $OPERATING_SYSTEM in
+while [[ $# -gt 1 ]]; do
+case "$1" in
+-u)
+    REMOTE_USER="$2"
+    shift
+	;;
+-i)
+    REMOTE_HOST="${2//,}"
+    shift
+	;;
+esac
+shift
+done
 
+case $REMOTE_OPERATING_SYSTEM in
 Fedora)
+	if [ -n "$REMOTE_HOST" ]; then
+		ssh ${REMOTE_USER:-$USER}@$REMOTE_HOST dnf install -y python
+	fi
     sudo dnf -y install ansible
-    ANSIBLE_OPTIONS+=bootstrap.yml
-;;
-
+    ANSIBLE_OPTIONS+=" bootstrap.yml"
+	;;
 esac
 
-ansible-playbook -i hosts $ANSIBLE_OPTIONS $@ site.yml
+ansible-playbook -i hosts $ANSIBLE_OPTIONS site.yml
