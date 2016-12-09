9f280c92658f24f4126973766b8f3da6ded34892
Roy Hyunjin Han <rhh@crosscompute.com>
2016-12-09 14:11:48 -0500

    Make it work if pip does not exist

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 OS_FAMILY=${OS_FAMILY:-RedHat}
-sudo pip install ansible
+
+if [ -n "$(command -v dnf)" ]; then
+    sudo dnf -y install ansible
+elif [ -n "$(command -v apt-get)" ]; then
+    sudo apt-get -y install ansible
+fi
+
 ansible-playbook -i hosts $@ bootstrap-${OS_FAMILY,,}.yml site.yml
