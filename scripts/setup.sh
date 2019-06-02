8255dc05a355ec73b7c9c96ee923e8ee36ad5914
Roy Hyunjin Han <rhh@crosscompute.com>
2019-06-02 15:18:28 -0400

    Skip bootstrap for Fedora 30

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,7 @@
-OS_FAMILY=${OS_FAMILY:-RedHat}
-
 if [ -n "$(command -v dnf)" ]; then
     sudo dnf -y install ansible
+    ansible-playbook -i hosts $@ site.yml
 elif [ -n "$(command -v apt-get)" ]; then
     sudo apt-get -y install ansible
+    ansible-playbook -i hosts $@ bootstrap-debian.yml site.yml
 fi
-
-ansible-playbook -i hosts $@ bootstrap-${OS_FAMILY,,}.yml site.yml
